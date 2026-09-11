e287a9f48d22a27d8c739048c675567eaf14f946
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2026-09-11 10:52:55 +0200

    add new tasks

---
+++ 10-style-binding/main.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    setup() {
+        const guitars = [
+            {id: 1, name: "Fender Stratocaster", stock: 0},
+            {id: 2, name: "Gibson Les Paul", stock: 3},
+            {id: 3, name: "Ibanez RG", stock: 5},
+            {id: 4, name: "ESP Eclipse", stock: 0},
+            {id: 5, name: "PRS Custom 24", stock: 2}
+        ]
+        const outOfStockColor = "red"
+        const OnlyFewLeftColor = "orange"
+        const inStockColor = "lightgreen"
+        return { guitars, outOfStockColor, OnlyFewLeftColor, inStockColor}
+    }
+})
+
+app.mount("#appdiv")
