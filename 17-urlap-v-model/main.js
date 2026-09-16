7843167e6920dc9e8acaba2be52557cbb36fdf81
BarnaCsaba_PG <barna.csaba@diak.szbi-pg.hu>
2026-09-16 13:55:51 +0200

    c

--- 17-urlap-v-model/main.js
+++ 17-urlap-v-model/main.js
@@ -1,27 +1,12 @@
 const app = Vue.createApp({
     setup() {
-        const price = Vue.ref(0)
+        const name = Vue.ref("")
 
-        const myMoney = Vue.ref(5)
-        const errorMessage = Vue.ref(null)
-        function increasePrice() {
-            price.value++
+        function updateName(event) {
+            name.value = event.target.value
         }
-        const watch = Vue.watch
-        function decreasePrice() {
-            price.value--
-        }
-        const formattedPrice = Vue.computed(() => {
-            return price.value.toLocaleString('en-US', { style: 'currency', currency: 'EUR' })
-        })
 
-        watch(price, () => {
-            errorMessage.value = price.value > myMoney.value ? "You don't have enough money!" : null
-        })
-        // return { price, increasePrice, decreasePrice }
-    // return { price, increasePrice, decreasePrice, formattedPrice     
-    
-    return { price, increasePrice, decreasePrice, formattedPrice, myMoney, errorMessage, watch }
+        return { name, updateName }
     }
 })
 
