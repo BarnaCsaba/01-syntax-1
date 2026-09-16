90a4661fefc5761d54a2c83cea4f1f53c87a12cf
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2026-09-16 13:59:31 +0200

    refactor urlap

--- 17-urlap-v-model/main.js
+++ 17-urlap-v-model/main.js
@@ -2,16 +2,7 @@ const app = Vue.createApp({
     setup() {
         const name = Vue.ref('')
 
-        // setTimeout(() => {
-        //     name.value = 'Szabolcs'
-        // }, 2000)
-
-        // function updateName(event) {
-        //     name.value = event.target.value
-        // }
-
         return { name,
-            // updateName
          }
     }
 })
