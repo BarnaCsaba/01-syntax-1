52bdb4068747d209dabac6db9c0210c11980da5c
BarnaCsaba_PG <barna.csaba@diak.szbi-pg.hu>
2026-09-16 13:39:00 +0200

    v

--- 16-watch/main.js
+++ 16-watch/main.js
@@ -2,22 +2,26 @@ const app = Vue.createApp({
     setup() {
         const price = Vue.ref(0)
 
-        
-
+        const myMoney = Vue.ref(5)
+        const errorMessage = Vue.ref(null)
         function increasePrice() {
             price.value++
         }
-
+        const watch = Vue.watch
         function decreasePrice() {
             price.value--
         }
         const formattedPrice = Vue.computed(() => {
             return price.value.toLocaleString('en-US', { style: 'currency', currency: 'EUR' })
         })
+
+        watch(price, () => {
+            errorMessage.value = price.value > myMoney.value ? "You don't have enough money!" : null
+        })
         // return { price, increasePrice, decreasePrice }
     // return { price, increasePrice, decreasePrice, formattedPrice     
     
-    return { price, increasePrice, decreasePrice, formattedPrice }
+    return { price, increasePrice, decreasePrice, formattedPrice, myMoney, errorMessage, watch }
     }
 })
 
